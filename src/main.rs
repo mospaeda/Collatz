29870a0b8409afcac4b469855b0de3830c7b9cfb
mospaeda <mospaeda@users.noreply.github.com>
2022-10-20 23:31:08 +0200

    rr

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
+
+
+use num_bigint::{ToBigInt, RandBigInt};
+
+let mut rng = rand::thread_rng();
+let a = rng.gen_bigint(1000);
+
+let low = -10000.to_bigint().unwrap();
+let high = 10000.to_bigint().unwrap();
+let b = rng.gen_bigint_range(&low, &high);
+
+// Probably an even larger number.
+println!("{}", a * b);
 }
+

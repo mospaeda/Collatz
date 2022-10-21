4276cd2c30f762dc954ee1bff607d2625400b8bc
mospaeda <mospaeda@users.noreply.github.com>
2022-10-21 20:45:16 +0200

    test

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,38 @@
+
+
 fn main() {
-use num_bigint::{ToBigInt, RandBigInt};
 
-let mut rng = rand::thread_rng();
-let a = rng.gen_bigint(1000);
+    use num_bigint::{ToBigInt};
+    use num_bigint::BigInt;
 
-let low = -10000.to_bigint().unwrap();
-let high = 10000.to_bigint().unwrap();
+        let one = 1.to_bigint().unwrap();
+let nb_max:u128=10;
 
-let b = rng.gen_bigint_range(&low, &high);
+println!("{}", one);
 
-// Probably an even larger number.
-println!("{}", a * b);
+// usage of for loop
+    for iii in 1..nb_max {
+        // A counter variable
+        let mut n:BigInt; 
+        let mut counter:u128 =0;
+
+        n =iii.to_bigint().unwrap();
+        // .to_bigint();
+ 
+
+    // Loop while `n` is less than 101
+    while  n > 1.to_bigint().unwrap() {
+        if &n%2 == 0.to_bigint().unwrap() {
+            n=n/2.to_bigint().unwrap();
+        }
+        else {
+            n=3.to_bigint().unwrap()*n+1.to_bigint().unwrap()
+        }
+        // Increment counter
+        counter += 1;
+
+    }
+    println!("number: {}  -  count: {}", iii, counter );
 }
+}
+
